a33e83ee2ae190d4e2328995bf2665e069c1e226
EC2 Default User <ec2-user@ip-172-31-85-129.ec2.internal>
2024-11-24 03:00:22 +0000

    change all

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 variable "vpc_name" {
     type = string
-    default = "vpc_clc12_terraform_iac"
+    default = "vpc_clc12_terraform_iac2"
 }
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
